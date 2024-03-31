b52052933f4d9a28d845ebebe9d9a22ad8db282e
chandrima925 <chandrimasengupta925@gmail.com>
2024-03-31 23:54:14 +0530

    updating

--- main.js
+++ main.js
@@ -14,3 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(changeImage, 1000);
 });
   
+// data counter
+(() => {
+    const counter = document.querySelectorAll('.counter');
+    // covert to array
+    const array = Array.from(counter);
+    // select array element
+    array.map((item) => {
+        // data layer
+        let counterInnerText = item.textContent;
+
+        let count = 1;
+        let speed = item.dataset.speed / counterInnerText
+        function counterUp() {
+            item.textContent = count++
+            if (counterInnerText < count) {
+                clearInterval(stop);
+            }
+        }
+        const stop = setInterval(() => {
+            counterUp();
+        }, speed);
+    })
+})();
