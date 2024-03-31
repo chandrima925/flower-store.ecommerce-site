f7d53a74f2c3cdb289a97d1dc364bd43290ecaba
chandrima925 <chandrimasengupta925@gmail.com>
2024-03-31 19:17:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// changing hero-section images
+document.addEventListener("DOMContentLoaded", function () {
+    var images = ["img/banner-img1.png", "img/banner-img2.png", "img/banner-imh3.png"]; // List of image URLs
+    var currentIndex = 0;
+    var imageContainer = document.getElementById("image-container");
+
+    // Function to change the image
+    function changeImage() {
+        imageContainer.querySelector("img").src = images[currentIndex];
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    // Change image every 5 seconds (5000 milliseconds)
+    setInterval(changeImage, 1000);
+});
